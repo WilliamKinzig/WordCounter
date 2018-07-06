d4b89fa3ddc309b47d9c4d690adbbaef2872fa33
WilliamKinzig <wippawill2@gmail.com>
2018-07-05 23:21:08 -0700

    refacter code. Comment out and hide unused code to reference later

--- WordCounter.Tests/ModelTests/RepeatCounterTest.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTest.cs
@@ -6,49 +6,83 @@ namespace WordCounter.Tests
     [TestClass]
     public class WordSearcherTest
     {
-        // [TestMethod]
-        // public void GetSetWordToFind_ReturnsWordToFind_String()
-        // {
-        //     //Arrange
-        //     string wordToCount = "word";
-        //     WordSearcher newWordSearcher = new WordSearcher(wordToCount);
-        //
-        //     //Act
-        //     string updatedWordCount= "blah";
-        //     newWordSearcher.SetWordCount(updatedWordCount);
-        //     string result = newWordSearcher.GetWordToCount();
-        //
-        //     //Assert
-        //     Assert.AreEqual(updatedWordToFind, result);
-        // }
-        //
-        // [TestMethod]
-        // public void GetSetParaGraphToScan_ReturnParagraphToScan_String()
-        // {
-        //     //Arrange
-        //     string paragraphToScanThrough = "para to scan through.";
-        //     WordSearcher newWordSearcher = new WordSearcher(paragraphToScanThrough);
-        //
-        //     //Act
-        //     string newParagraph = "Scooby Dooby Doo. Where are you?";
-        //     newWordSearcher.SetParagraph(newParagraph);
-        //     string result = newWordSearcher.GetParaGraph();
-        //
-        //     //Assert
-        //     Assert.AreEqual(newParagraph, result);
-        // }
-
         [TestMethod]
         public void GetStringToCount_ReturnStringsWorkingWith_String()
         {
             //Arrange
             WordSearcher newWordSearcher = new WordSearcher("the","the yellow bus");
+
             //Act
             string toCount = newWordSearcher.GetWordToCount();
             string toScan = newWordSearcher.GetParaGraph();
+            
             //Assert
             //Assert.AreEqual(toCount, "the");
             Assert.AreEqual(toScan, "the yellow bus");
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [TestMethod]
+// public void GetSetWordToFind_ReturnsWordToFind_String()
+// {
+//     //Arrange
+//     string wordToCount = "word";
+//     WordSearcher newWordSearcher = new WordSearcher(wordToCount);
+//
+//     //Act
+//     string updatedWordCount= "blah";
+//     newWordSearcher.SetWordCount(updatedWordCount);
+//     string result = newWordSearcher.GetWordToCount();
+//
+//     //Assert
+//     Assert.AreEqual(updatedWordToFind, result);
+// }
+//
+// [TestMethod]
+// public void GetSetParaGraphToScan_ReturnParagraphToScan_String()
+// {
+//     //Arrange
+//     string paragraphToScanThrough = "para to scan through.";
+//     WordSearcher newWordSearcher = new WordSearcher(paragraphToScanThrough);
+//
+//     //Act
+//     string newParagraph = "Scooby Dooby Doo. Where are you?";
+//     newWordSearcher.SetParagraph(newParagraph);
+//     string result = newWordSearcher.GetParaGraph();
+//
+//     //Assert
+//     Assert.AreEqual(newParagraph, result);
+// }
